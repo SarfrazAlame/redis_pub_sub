f9887465dfb713817e29d189b8bb35b596aaa576
Sarfraz Alam <sarfrazk7858@gmail.com>
2024-04-13 17:24:29 +0530

    sdg

--- express-backend/src/index.ts
+++ express-backend/src/index.ts
@@ -6,11 +6,20 @@ const app = express()
 const client = createClient()
 client.connect()
 
-app.post("/submit", (req, res) => {
-    const { problemId, userId, code, language } = req.body
+app.post("/submit", async (req, res) => {
+    const { problemId, code, language, userId } = req.body
     // push this to a database prisam.submission.create()
-    client.lPush("submissions", JSON.stringify({ problemId, userId, code, language }))
-    res.json({
-        message: "Submission received!"
-    })
+    console.log("hii")
+    try {
+        await client.lPush("submissions", JSON.stringify({ problemId, userId, code, language }))
+        return res.json({
+            message: "Submission received!"
+        })
+    } catch (error) {
+        return res.json({
+            message:"Submission failed"
+        })
+    }
 })
+
+app.listen(3000)
